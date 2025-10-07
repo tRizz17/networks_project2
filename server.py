241d4ee54bb6762b01c99f6c6a9c0c61dea1765d
Thomas Rizzo <rizzot@oregonstate.edu>
2025-10-06 19:07:52 -0700

    got lost in the sauce, added a lot since last commit. Almost got it

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 from urllib.parse import urlparse
+import socket
 import os
 import argparse
-
-def extractSchemeAndPath(url):
-    url = urlparse(url)
-    scheme = url.scheme
-    return (url.scheme, url.path)
+import mimetypes
 
 
-def getMIMEType(path):
-    split_the_path = os.path.split(path)
-    get_mime = os.path.splitext(split_the_path[-1])
-    mime = get_mime[-1]
-    return mime
+def splitAndParse(header):
+    first_line = header[0].split()
+    parsed = urlparse(first_line[1])
+    full_path = parsed.path
+    method = first_line[0]
+    version = first_line[2]
+    file_name = os.path.split(full_path)[-1]
+    return method, file_name, version
 
 
 def WebServer(port):
@@ -21,10 +21,10 @@ def WebServer(port):
     s.bind(('', port))
     s.listen()
     while True:
-        print("Listening for incoming connections...")
+        print("Listening for incoming connections...\r\n")
         new_conn = s.accept()
         client_addr = new_conn[1][0]
-        print(f"IP Address: {client_addr}")
+        # print(f"IP Address: {client_addr}")
         new_socket = new_conn[0]
         buffer = ""
         while True:
@@ -32,21 +32,44 @@ def WebServer(port):
             buffer += request_data
             if "\r\n\r\n" in buffer:
                 print(buffer)
-                break
+                header = buffer.split("\r\n")
+                method, file_name, version = splitAndParse(header)
+                mimetype, _ = mimetypes.guess_type(file_name)
+                if mimetype is None:
+                    mimetype = "application/octet-stream"
+                print(method, file_name, version, mimetype)
+                try:
+                    with open(file_name, "rb") as fp:
+                        data = fp.read()
+                        print(data)
+                        content_length = data.length()
+                        response = (
+                            f"HTTP/1.1 200 OK\r\n"
+                            f"Content-Type: {mimetype}\r\n"
+                            f"Content-Length: {content_length}\r\n"
+                            f"Connection: close\r\n\r\n{data}\r\n\r\n"
+                        )
+                        new_socket.sendall(response.encode("ISO-8859-1"))
+                        new_socket.close()
+                except:
+                    not_found_response = (
+                        "HTTP/1.1 404 Not Found\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Content-Length: 13\r\n"
+                        "Connection: close\r\n\r\n404 Not Found"
+                    )
+                    new_socket.sendall(not_found_response.encode("ISO-8859-1"))
+                    new_socket.close()
+                    break
 
-
-
-mapping = {
-    "html": "text/html",
-    "txt": "txt/plain"
-}
+                break   
 
 parser = argparse.ArgumentParser(description="Specify a port number if you wish")
 parser.add_argument("port", nargs="?", type=int, default=28333, help="Enter a valid port number")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    WebServer.py(port)
+    WebServer(args.port)
 
 # Parse that request header to get the file name.
 # Strip the path off for security reasons.
@@ -79,3 +102,4 @@ if __name__ == "__main__":
 
 # if mimeType is None:
 #     #application/octet-stream
+
